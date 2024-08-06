962e994c4b8d20f89c622f4f0cd20ebfacbed454
Thota Phaneendhar <babithota5@gmail.com>
2024-08-06 18:38:49 +0530

    changed certifications section

--- src/sections/Certifications.js
+++ src/sections/Certifications.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import './Resume.css';
+import React from "react";
+import "./Certifications.css";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Certifications = () => {
+    const certificates = [
+        { title: "Frontend Developer (React) Role Certificate from Hacker Rank", link: "https://www.hackerrank.com/certificates/6a23b41a066f" },
+        { title: "React Basic Skill Assessment Certificate from Hacker Rank", link: "https://www.hackerrank.com/certificates/deaff7eccd06" },
+        { title: "Angular Basic Skill Assessment Certificate from Hacker Rank", link: "https://www.hackerrank.com/certificates/b7bc68149fe8" },
+        { title: "Javascript Basic Skill Assessment Certificate from Hacker Rank", link: "https://www.hackerrank.com/certificates/a57007e01c10" },
+        { title: "Problem Solving Basic Skill Assessment Certificate from Hacker Rank", link: "https://www.hackerrank.com/certificates/364758c56eaf" },
+        { title: "SQL Basic Skill Assessment Certificate from Hacker Rank", link: "https://www.hackerrank.com/certificates/1e0ef361a911" }
+
+      ];
   return (
     <div id="certifications" className="certifications">
-      <h1>Resume</h1>
-      <section>
-        <h2>Work Experience</h2>
-        <p>Details about your work experience.</p>
-      </section>
-      <section>
-        <h2>Education</h2>
-        <p>Details about your education.</p>
-      </section>
-      <section>
-        <h2>Skills</h2>
-        <p>Details about your skills.</p>
-      </section>
+      <h1>Certifications</h1>
+      <ListGroup>
+        {certificates.map((cert, index) => (
+          <ListGroup.Item variant="info" key={index}>
+            <a href={cert.link} target="_blank" rel="noopener noreferrer">
+              {cert.title}
+            </a>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
-}
+};
 
 export default Certifications;
